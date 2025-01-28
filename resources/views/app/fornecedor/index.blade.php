06ceb5de1913c269d5956124e8b7f791160c347b
rib1908 <rib.js198@gmail.com>
2025-01-28 16:43:25 -0300

    Contato Migration

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,12 @@
+<h3>Fornecedor</h3>
+
+{{ 'Texto de teste' }}
+<?= 'Texto de teste' ?
+
+{{--Comentario --}}
+
+@php
+    /*
+    Comentarios de multiplas linhas
+    */
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'Contato'])->name('site.contato');
+Route::post('/contato',[\App\Http\Controllers\ContatoController::class, 'Contato'])->name('site.contato');
+Route::get('/login',[\App\Http\Controllers\ContatoController::class, 'Login'])->name('site.login');
+
+
+Route::prefix('/app')->group( function(){
+    Route::get('/clientes',[\App\Http\Controllers\ContatoController::class, 'Clientes'])->name('app.clientes');
+    Route::get('/fornecedores',[\App\Http\Controllers\FornecedorController::class, 'Fornecedores'])->name('app.fornecedores');
+    Route::get('/produtos',[\App\Http\Controllers\ContatoController::class, 'produtos'])->name('site.produtos');
+});
+
+
+Route::get('/teste/{p1}/{p2}',[\App\Http\Controllers\TesteController::class, 'Teste'])->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a>';
 });
